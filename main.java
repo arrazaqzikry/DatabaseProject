75b790b48f68857afb3a0e0e9dec8ddd9df2ec30
arrazaq zikry <zikryarrazaq@gmail.com>
2025-06-17 16:43:55 +0800

    1

--- main.java
+++ main.java
@@ -1,6 +1,7 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("lol");
-        System.out.println("lol");
+        System.out.println("lol1");
+        System.out.println("lol2");
+        System.out.println("lol3");
     }
 }
